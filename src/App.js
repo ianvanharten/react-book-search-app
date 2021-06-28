e4af66b07345a6c88f4addeca03a3e921e4426da
ianvanharten <ianvharten@gmail.com>
2021-06-28 19:41:29 -0400

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Header from './Header'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello!</h1>
+    <div>
+        <Header />
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/Header.js
@@ -0,0 +1,7 @@
+function Header() {
+    return(
+        <h1>Book Search App</h1>
+    )
+}
+
+export default Header
