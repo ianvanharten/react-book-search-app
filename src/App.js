55c215f4131b6eb103ab70ae4ea18eeed5321a47
ianvanharten <ianvharten@gmail.com>
2021-06-28 20:00:33 -0400

    Move getBookData to BookSearch component

--- src/App.js
+++ src/App.js
@@ -3,23 +3,12 @@ import Header from './Header'
 import BookSearch from './BookSearch'
 
 function App() {
-    const [ searchResults, setSearchResults ] = useState('')
   return (
     <div>
         <Header />
-        <BookSearch onClick={getBookData}/>
+        <BookSearch />
     </div>
   );
-}
-
-function getBookData() {
-
-    fetch('https://www.googleapis.com/books/v1/volumes?q=clean+coder')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data.items[0].volumeInfo)
-            // setResult(data.items[0].volumeInfo)
-        })
 }
 
 export default App
--- src/BookSearch.js
+++ src/BookSearch.js
@@ -1,10 +1,25 @@
+import {useState} from "react";
+
 function BookSearch(props) {
+    const [ searchResults, setSearchResults ] = useState('')
     return(
         <div>
             <input type="text" />
-            <button onClick={props.onClick}>Search</button>
+            <button onClick={getBookData}>Search</button>
         </div>
     )
+
+    function getBookData() {
+
+        fetch('https://www.googleapis.com/books/v1/volumes?q=clean+coder')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.items[0].volumeInfo.title)
+                // setSearchResults(data.items[0].volumeInfo)
+            })
+    }
 }
+
+
 
 export default BookSearch
