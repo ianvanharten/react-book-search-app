1bff7a73120196a462af396bb95ce76d6abfff71
ianvanharten <ianvharten@gmail.com>
2021-07-07 21:48:23 -0400

    Update styles on modal

--- src/BookCard.js
+++ src/BookCard.js
@@ -17,6 +17,38 @@ function BookCard(props) {
         justifyContent: 'space-around'
     }
 
+    Modal.defaultStyles = {
+        overlay: {
+            position: 'fixed',
+            top: '0',
+            left: '0',
+            right: '0',
+            bottom: '0',
+            backgroundColor: 'rgba(255, 255, 255, 0.75)'
+        },
+        content: {
+            position: 'absolute',
+            inset: '40px',
+            border: '1px solid rgb(204, 204, 204)',
+            background: 'rgb(255, 255, 255)',
+            overflow: 'auto',
+            borderRadius: '4px',
+            outline: 'none',
+            padding: '40px',
+            width: '70%',
+            margin: '0 auto',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+        }
+    }
+
+    const descriptionStyles = {
+        lineHeight: '2',
+        fontSize: '16px',
+        width: '60%'
+    }
+
     function openModal() {
         setIsOpen(true)
     }
@@ -35,8 +67,9 @@ function BookCard(props) {
             </div>
             <Modal isOpen={modalIsOpen}>
                 <h2>{props.book.title}</h2>
+                <img src={props.book.image} alt={props.book.title + 'cover image'} style={{width: '12%'}}/>
                 <p>{props.book.author[0]}</p>
-                <img src={props.book.image} alt={props.book.title + 'cover image'}/>
+                <p style={descriptionStyles}>{props.book.description}</p>
                 <button onClick={closeModal}>Close</button>
             </Modal>
         </div>
