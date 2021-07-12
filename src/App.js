ecbe21b1ada707e7430d02783e558cca2a5af9fb
ianvanharten <ianvharten@gmail.com>
2021-07-12 13:11:11 -0400

    Move App component styles to index.css

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import React from 'react'
 import BookSearch from './BookSearch'
 
 function App() {
-    const appStyles = {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center'
-    }
 
   return (
-    <div style={appStyles}>
+    <div className='app'>
         <h1>Book Search App</h1>
         <BookSearch />
     </div>
